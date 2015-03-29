c1f9f026f840e0110c261c2a7fe7cb4c7c9c1ad6
Katy Ereira <kachii@gmail.com>
2015-03-29 12:03:09 +0100

    Move plant status HTML and JS to separate plant status component
     - Add plant-status element directive
     - Add editable attribute directive to plant-status directive
     - Add/remove edit status form depending on editable status

---
+++ components/plants/status/plantStatus.js
@@ -0,0 +1,51 @@
+angular.module('gardenTracker').directive('plantStatus', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'components/plants/status/plant-status.html',
+        controller: function ($scope) {
+            $scope.editable = false;
+
+            $scope.statusLabel = function (status) {
+                for (var i in $scope.statuses) {
+                    if ($scope.statuses[i].name == status) {
+                        return $scope.statuses[i].class;
+                    }
+                }
+            };
+
+            $scope.editStatus = function (plant) {
+                plant.editing = true;
+            };
+
+            $scope.cancelEditStatus = function (plant) {
+                plant.editing = false;
+            };
+
+            $scope.updateStatus = function (plant) {
+                plant.statuses.push(
+                    {
+                        name: plant.status.name,
+                        date: new Date()
+                    }
+                );
+
+                plant.editing = false;
+                plant.status = false;
+            };
+
+            $scope.editingStatus = function (plant) {
+                return plant.editing;
+            }
+        },
+        controllerAs: 'plantStatusCtrl'
+    }
+})
+.directive('editable', function() {
+  return {
+      require: '^plantStatus',
+      restrict : 'A',
+      link: function($scope) {
+          $scope.editable = true;
+      }
+  }
+});
