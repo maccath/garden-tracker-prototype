c57cec3c08a546fbdb8234f429cad9b667ef67e2
Katy Ereira <kachii@gmail.com>
2015-03-29 11:32:10 +0100

    Move plant stats HTML and JS to separate plants stats component

---
+++ components/plants/stats/plantStats.js
@@ -0,0 +1,6 @@
+angular.module('gardenTracker').directive('plantStats', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'components/plants/stats/plant-stats.html'
+    }
+});
